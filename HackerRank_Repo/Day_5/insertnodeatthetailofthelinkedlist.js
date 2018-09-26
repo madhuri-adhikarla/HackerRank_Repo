8d0056c9d1071d5b3fe8b32ec88ea0a83c58f8d8
Madhuri Adhikarla <madhuri.a@techaspect.com>
2018-09-27 00:25:30 +0530

    insert node at the tail  position in linked listsource code

---
+++ HackerRank_Repo/Day_5/insertnodeatthetailofthelinkedlist.js
@@ -0,0 +1,31 @@
+// Complete the insertNodeAtTail function below.
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode next;
+ * }
+ *
+ */
+function insertNodeAtTail(head, data) {
+    
+    const node = new SinglyLinkedListNode(data);
+
+            if(head==null){
+             node.next = null;   
+             head=node;
+             return head;
+        }
+         else{
+             var current = head;
+            while(current.next!=null){
+                current = current.next;
+            }
+            current.next=node;
+            return head;
+        }
+
+
+}
