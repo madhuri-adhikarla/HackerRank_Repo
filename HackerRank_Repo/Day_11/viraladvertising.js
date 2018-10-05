1641e15bf281b91a1848e089cfecd6b12bd6a9b0
Madhuri Adhikarla <madhuri.a@techaspect.com>
2018-10-05 18:51:08 +0530

    source code for viral advertising in java script

---
+++ HackerRank_Repo/Day_11/viraladvertising.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the viralAdvertising function below.
+function viralAdvertising(n) {
+        var people = 2;
+        var count  = 2;
+        for (let i = 2; i <= n; i++) {
+            people = Math.floor((people * 3) / 2);
+            count += people;
+        }
+        return (count);
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    let result = viralAdvertising(n);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
