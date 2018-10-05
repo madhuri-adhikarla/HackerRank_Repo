564a9b9a54a6dcc3c260d09f306a29b1b8ab48e8
Madhuri Adhikarla <madhuri.a@techaspect.com>
2018-10-05 17:02:08 +0530

    source code for design pdf viewer in javascript

---
+++ HackerRank_Repo/Day_11/designpdfviewer.js
@@ -0,0 +1,52 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the designerPdfViewer function below.
+function designerPdfViewer(h, word) {
+    var tallestLetter=0;
+    for (var i = 0; i < word.length; ++i) {
+        var height = word.charCodeAt(i);
+        var val = height - 97;
+        if(h[val]>tallestLetter) {
+            tallestLetter=h[val];
+        }
+    }
+    return (tallestLetter*word.length);
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const h = readLine().split(' ').map(hTemp => parseInt(hTemp, 10));
+
+    const word = readLine();
+
+    let result = designerPdfViewer(h, word);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
