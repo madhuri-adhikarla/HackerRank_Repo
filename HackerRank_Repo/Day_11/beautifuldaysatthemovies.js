8cb468bcb60d1395a2dda468b532e52826d54560
Madhuri Adhikarla <madhuri.a@techaspect.com>
2018-10-05 18:44:44 +0530

    source code for beautiful days at the movies in javascript

---
+++ HackerRank_Repo/Day_11/beautifuldaysatthemovies.js
@@ -0,0 +1,63 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the beautifulDays function below.
+function beautifulDays(i, j, k) {
+    var beautifulDay=0;
+    for(let count = i;count<=j;count++) {
+        var rev =reverse_a_number(count); 
+        var val = (Math.abs(count-rev));
+        // console.log(val);
+        if(val % k == 0) {
+           beautifulDay++; 
+        }
+    }
+    return beautifulDay;
+}
+
+function reverse_a_number(n)
+{
+    n = n + "";
+    return n.split("").reverse().join("");
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const ijk = readLine().split(' ');
+
+    const i = parseInt(ijk[0], 10);
+
+    const j = parseInt(ijk[1], 10);
+
+    const k = parseInt(ijk[2], 10);
+
+    let result = beautifulDays(i, j, k);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
