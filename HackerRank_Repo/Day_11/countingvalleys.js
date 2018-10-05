454c62bb80b61139c6aa385739c9540318b9c827
Madhuri Adhikarla <madhuri.a@techaspect.com>
2018-10-05 11:18:42 +0530

    source code for counting valleys in javascript

---
+++ HackerRank_Repo/Day_11/countingvalleys.js
@@ -0,0 +1,59 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the countingValleys function below.
+function countingValleys(n, s) {
+    var charS = [];
+    charS= s.split('');
+    var down=0;
+    var up=0;
+    var valleys=0;
+    for(let i = 0; i < charS.length; i++) {
+        if(charS[i] == 'D') {
+            down++;
+        } else if(charS[i] == 'U'){
+            up++;
+        }
+        if(down==up && charS[i]=='U') {
+            valleys++;
+        }
+    }
+    return valleys;
+
+}
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    const s = readLine();
+
+    let result = countingValleys(n, s);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
