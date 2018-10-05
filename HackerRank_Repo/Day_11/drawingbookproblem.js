768c8bd8405aded843865f2a909800f366314dc3
Madhuri Adhikarla <madhuri.a@techaspect.com>
2018-10-05 10:30:55 +0530

    source code for sock drawing book problem in javascript

---
+++ HackerRank_Repo/Day_11/drawingbookproblem.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(str => str.trim());
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the pageCount function below.
+ */
+function pageCount(n, p) {
+     var firstPage = Math.floor(p / 2),
+      lastPage = Math.floor(n / 2) - firstPage;
+    if(firstPage>lastPage)
+        return lastPage;
+    else
+        return firstPage;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    const p = parseInt(readLine(), 10);
+
+    let result = pageCount(n, p);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
