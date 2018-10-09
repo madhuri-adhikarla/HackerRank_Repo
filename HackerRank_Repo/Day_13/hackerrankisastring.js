162440a24d3361c5797598bc52eaee27a820301a
Madhuri Adhikarla <madhuri.a@tadigital.com>
2018-10-09 13:50:04 +0530

    Added source code for hackerrank is a string challenge in javascript

---
+++ HackerRank_Repo/Day_13/hackerrankisastring.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the hackerrankInString function below.
+function hackerrankInString(s) {
+    var str = "hackerrank";
+    var index=0;
+    for(var i=0 ; i<s.length;++i){
+        if(str[index]==s[i]){
+           index++;
+        }
+    }
+    if(index!=str.length){
+     return "NO"  ; 
+    }
+    
+    return "YES";
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const q = parseInt(readLine(), 10);
+
+    for (let qItr = 0; qItr < q; qItr++) {
+        const s = readLine();
+
+        let result = hackerrankInString(s);
+
+        ws.write(result + "\n");
+    }
+
+    ws.end();
+}
