2d83a9718f563b672816c002e3349b214d2d2c8b
Madhuri Adhikarla <madhuri.a@tadigital.com>
2018-10-09 17:02:05 +0530

    Added source code for alternating characters challenge in javascript

---
+++ HackerRank_Repo/Day_13/alternatingcharacters.js
@@ -0,0 +1,64 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the alternatingCharacters function below.
+function alternatingCharacters(s) {
+    var last = s.charAt(0);
+    var count = 0;
+    for(var i = 1; i < s.length; i++) {
+        if(last === s.charAt(i)) {
+            count++;
+        } else {
+            last = s.charAt(i);
+        }
+    }
+    return count;
+}
+
+function arrayRemove(arr, value) {
+
+   return arr.filter(function(ele){
+       return ele != value;
+   });
+
+}
+
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const q = parseInt(readLine(), 10);
+
+    for (let qItr = 0; qItr < q; qItr++) {
+        const s = readLine();
+
+        let result = alternatingCharacters(s);
+
+        ws.write(result + "\n");
+    }
+
+    ws.end();
+}
