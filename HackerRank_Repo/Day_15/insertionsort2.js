8d6d9b29ec2b5f0f40a1aa3eca9bc2e699988df3
Madhuri Adhikarla <madhuri.a@tadigital.com>
2018-10-11 14:44:57 +0530

    source code for insertion sort part 2 in javascript

---
+++ HackerRank_Repo/Day_15/insertionsort2.js
@@ -0,0 +1,50 @@
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the insertionSort2 function below.
+function insertionSort2(n, arr) {
+   for(let i=0;i<n-1;i++) {
+          var j = i+1;
+          while(j>0) {
+            if(arr[j]<arr[j-1]) {
+              var temp = arr[j];
+              arr[j] = arr[j-1];
+              arr[j-1] = temp;
+              j--;
+            } else {
+                break;
+            }
+          }
+          console.log(arr.join(' '));
+   }
+    
+}
+
+function main() {
+    const n = parseInt(readLine(), 10);
+
+    const arr = readLine().split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    insertionSort2(n, arr);
+}
