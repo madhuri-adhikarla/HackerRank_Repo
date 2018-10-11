e4e8bde9f7f3ce98d377e21e78acd20369bc5592
Madhuri Adhikarla <madhuri.a@tadigital.com>
2018-10-11 17:50:59 +0530

    source code for running time of algorithms in javascript

---
+++ HackerRank_Repo/Day_15/runningtimeofalgorithms.js
@@ -0,0 +1,56 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the runningTime function below.
+function runningTime(arr) {
+    var numShifts = 0;
+    for ( var i = 1; i < arr.length; i++ ) {
+        var value = arr[i];
+        var j = i - 1;
+        while( j >= 0 && value < arr[j] ) {
+            arr[i] = arr[j];
+            arr[j] = value;
+            numShifts += 1;
+            j -= 1;
+            i -= 1;
+        }
+    }
+    return numShifts;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    const arr = readLine().split(' ').map(arrTemp => parseInt(arrTemp, 10));
+
+    let result = runningTime(arr);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
