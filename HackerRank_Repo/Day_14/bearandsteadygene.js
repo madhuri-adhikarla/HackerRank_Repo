e783bcc5b82a3fab5e8d42b826549160470e9317
Madhuri Adhikarla <madhuri.a@tadigital.com>
2018-10-12 17:07:29 +0530

    source code for bear and steady gene in java script

---
+++ HackerRank_Repo/Day_14/bearandsteadygene.js
@@ -0,0 +1,76 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the steadyGene function below.
+function steadyGene(str) {
+  let n = str.length;
+  let i=0, j=n-1, minl=n;
+  let cnt = { A: 0, C: 0, T: 0, G: 0 };
+  while (1)
+  {
+    if (j<0 || cnt[str[j]]==n/4)
+    {
+      j++;
+      break;
+    }
+    else
+    {
+      cnt[str[j]]++;
+      j--;
+    }
+  }
+  // console.log('cnt: ', cnt);
+  if (j < minl)
+    minl = j;
+  // console.log('minl: ', minl);
+  for (i=0; i<n; i++)
+  {
+    cnt[str[i]]++;
+    while (j<n && cnt[str[i]] > n/4)
+    {
+      cnt[str[j]]--;
+      j++;
+    }
+    if (j==n)
+      break;
+    if (j-i-1 < minl)
+      minl = j-i-1;
+  }
+  return minl;
+}
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    const gene = readLine();
+
+    let result = steadyGene(gene);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
