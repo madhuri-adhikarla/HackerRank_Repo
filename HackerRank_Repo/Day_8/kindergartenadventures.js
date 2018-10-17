6148ed726333321d90f198ea57a78f15193bf6ff
Madhuri Adhikarla <madhuri.a@tadigital.com>
2018-10-17 13:54:56 +0530

    source code for kinder garten adventures in javascript

--- HackerRank_Repo/Day_8/kindergartenadventures.js
+++ HackerRank_Repo/Day_8/kindergartenadventures.js
@@ -1,35 +1,68 @@
-function processData(input) {
-    var args = input.split('\n');
-    var N = Number(args[0]);
-    var t = args[1].split(' ').map(x => Number(x));
-    var c = t.map(x => 0);
-    
-    for (var i = 0; i < N; i++) {
-        if (t[i] < N) {
-            c[(i + 1) % N]++;
-            c[(i - t[i] + 1 + N) % N]--;
-        }
-    }
-    
+'use strict';
 
-    var pos, max = -1, curr = 0;
-    for (var i = 0; i < N; i++) {
-        curr += c[i];
-        if (curr > max) {
-            pos = i;
-            max = curr;
-        }
-    }
-    
-} 
+const fs = require('fs');
 
 process.stdin.resume();
-process.stdin.setEncoding("ascii");
-_input = "";
-process.stdin.on("data", function (input) {
-    _input += input;
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
 });
 
-process.stdin.on("end", function () {
-   processData(_input);
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(str => str.trim());
+
+    main();
 });
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the solve function below.
+ */
+function solve(t) {
+   
+    
+    let score = [];
+    for (let i = 0; i < t.length; i++) {
+        score[i] = 0;
+    }
+    for (let i = 0; i < t.length; i++) {
+        if (t[i] > 0 && t[i] < t.length) {
+            score[(i - t[i] + 1 + t.length) % t.length]--;
+            score[(i + 1) % t.length]++;
+        }
+    }
+    
+    let max = -1;
+    let indx = -1;
+    let sum = 0;
+    for (let i = 0; i < t.length; i++) {
+        sum += score[i];
+        if (sum > max) {
+            max = sum;
+            indx = i;
+        }
+    }
+    
+    return indx + 1;
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const tCount = parseInt(readLine(), 10);
+
+    const t = readLine().split(' ').map(tTemp => parseInt(tTemp, 10));
+
+    let id = solve(t);
+
+    ws.write(id + "\n");
+
+    ws.end();
+}
