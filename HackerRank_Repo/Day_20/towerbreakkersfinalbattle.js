5fe300581c3c0fbd2fafbedd0d57c935e769dc72
madhuri-adhikarla <adhikarlamadhuri66@gmail.com>
2018-10-18 12:18:57 +0530

    Update towerbreakkersfinalbattle.js

--- HackerRank_Repo/Day_20/towerbreakkersfinalbattle.js
+++ HackerRank_Repo/Day_20/towerbreakkersfinalbattle.js
@@ -1,54 +1,37 @@
-'use strict';
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+import java.util.Arrays;
 
-const fs = require('fs');
-
-process.stdin.resume();
-process.stdin.setEncoding('utf-8');
-
-let inputString = '';
-let currentLine = 0;
-
-process.stdin.on('data', inputStdin => {
-    inputString += inputStdin;
-});
-
-process.stdin.on('end', _ => {
-    inputString = inputString.replace(/\s*$/, '')
-        .split('\n')
-        .map(str => str.replace(/\s*$/, ''));
-
-    main();
-});
-
-function readLine() {
-    return inputString[currentLine++];
-}
-
-// Complete the towerBreakers function below.
-function towerBreakers(n, m) {
-    // Complete this function
-    if(m === 1 || n%2 === 0) {
-        return 2;
-    } else {
-        return 1;
+public class Solution {
+    private static long[] cost;
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        cost = new long[136];
+        cost[0] = 1;
+        cost[1] = 1;
+        cost[2] = 1;
+        cost[3] = 1;
+        cost[4] = 2;
+        for(int i = 5; i <= 127; i++){
+            long result = 0;
+            int limit = (int)(Math.sqrt(i) + 0.5);
+            for(int j = 1; j <= limit; j++){
+                if(j * j <= i)
+                    result += cost[i - j * j];
+            }
+            //if(result < 0) System.out.println(i);
+            cost[i] = result;
+        }
+        //System.out.println(cost[127]);
+        int ncases = Integer.parseInt(new StringTokenizer(br.readLine()).nextToken());
+        for(int i = 0; i < ncases; i++){
+            long n = Long.parseLong(new  StringTokenizer(br.readLine()).nextToken());
+            int pos = Arrays.binarySearch(cost, n);
+            if(pos < 0)
+                pos = -pos - 1;
+            System.out.println(pos);
+        }
     }
-}
-function main() {
-    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
-
-    const t = parseInt(readLine(), 10);
-
-    for (let tItr = 0; tItr < t; tItr++) {
-        const nm = readLine().split(' ');
-
-        const n = parseInt(nm[0], 10);
-
-        const m = parseInt(nm[1], 10);
-
-        let result = towerBreakers(n, m);
-
-        ws.write(result + "\n");
-    }
-
-    ws.end();
 }
